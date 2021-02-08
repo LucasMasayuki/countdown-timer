b18b1cfa469692241779c1bd4a0ac5dc503ae959
Lucas Masayuki <lutamaribuchi@gmail.com>
2021-02-08 00:36:04 -0300

    Navbar with dark mode

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,46 +1,35 @@
-<script lang="ts">
+<script>
     import SwitchButton from './SwitchButton.svelte'
+    export let title
     let darkMode = false;
-	let title = 'Countdown timer'
 </script>
 
 <style>
-	nav {
+    .header {
         background-color: royalblue;
         color: white;
-	}
-	ul {
-		align-items: center;
         display: inline-grid;
         grid-template-columns: 85% 15%;
-        width: -webkit-fill-available;
-	}
-
-	ul {
-		list-style: none;
-	}
-
-    .title-box {
-		text-transform: uppercase;
+        padding: 0.10em;
+        width: 100%;
     }
-
+    .title-box {
+        margin-left: 15px;
+    }
     .switch-dark-mode {
         align-items: center;
         display: inline-grid;
         float: right;
         grid-template-columns: auto auto;
     }
-
 </style>
 
-<nav>
-	<ul>
-		<li class="title-box">
-			<h2>{title}</h2>
-		</li>
-		<li class="switch-dark-mode">
-			<h3>dark mode</h3>
-			<SwitchButton bind:checked={darkMode} />
-		</li>
-	</ul>
-</nav>
+<header class="header">
+    <div class="title-box">
+        <h2>{title}</h2>
+    </div>
+    <div class="switch-dark-mode">
+        <h3>dark mode</h3>
+        <SwitchButton bind:checked={darkMode} />
+    </div>
+</header>
