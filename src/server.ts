102709b11b72fb934be025d6fb69f23c8ecc0e6c
Lucas Masayuki <lutamaribuchi@gmail.com>
2021-02-23 22:58:40 -0300

    Fix

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
-		'countdown-timer',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
